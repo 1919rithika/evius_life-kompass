3a579cda514ebcea8fe562378303fa30a32ce4a6
1919rithika <paidalarethikareddy@gmail.com>
2025-03-18 20:24:00 +0530

    changed

--- evius_life-kompass/next.config.js
+++ evius_life-kompass/next.config.js
@@ -3,8 +3,7 @@ const nextConfig = {
   eslint: {
     // This will ignore ESLint errors during builds
     ignoreDuringBuilds: true,
-  },
-  telemetry: false  // This disables telemetry
+  }
 };
 
 module.exports = nextConfig; 
