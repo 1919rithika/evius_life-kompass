409af8288bb625dbfb16362d34890ba05895a512
1919rithika <paidalarethikareddy@gmail.com>
2025-03-24 16:26:22 +0530

    chnaged the app theme

--- evius_life-kompass/components/ui/footer.tsx
+++ evius_life-kompass/components/ui/footer.tsx
@@ -1,4 +1,3 @@
-
 import Link from "next/link";
 
 
@@ -8,35 +7,15 @@ const Footer = () => {
     <footer className="w-full border-t border-gray-600 py-4 mt-40">
       <div className="container mx-auto px-4">
         <nav className="flex justify-center space-x-4 text-sm">
-          <Link
-            href="/footer/imprint"
-            className="text-gray-700 hover:text-gray-900 transition-colors"
-          >
-            Imprint
-          </Link>
-          <span className="text-gray-400">|</span>
-          <Link
-            href="/footer/terms"
-            className="text-gray-700 hover:text-gray-900  transition-colors"
-          >
-            Terms 
-          </Link>
-          <span className="text-gray-400">|</span>
-          <Link
-            href="/footer/privacy"
-            className="text-gray-700 hover:text-gray-900  transition-colors"
-          >
-            Privacy Policies
-          </Link>
-          <span className="text-gray-400">|</span>
-          <Link
-            href="/footer/cookies"
-            className="text-gray-700 hover:text-gray-900  transition-colors"
-          >
-            Cookie Policy
-          </Link>
-                </nav>
-                <p className="text-gray-700 text-center mt-4">© evius life/kompass 2025. All rights reserved.</p>
+          <a className="text-gray-400 hover:text-gray-300 transition-colors" href="/footer/imprint">Imprint</a>
+          <span className="text-gray-500">|</span>
+          <a className="text-gray-400 hover:text-gray-300 transition-colors" href="/footer/terms">Terms</a>
+          <span className="text-gray-500">|</span>
+          <a className="text-gray-400 hover:text-gray-300 transition-colors" href="/footer/privacy">Privacy Policies</a>
+          <span className="text-gray-500">|</span>
+          <a className="text-gray-400 hover:text-gray-300 transition-colors" href="/footer/cookies">Cookie Policy</a>
+        </nav>
+        <p className="text-gray-400 text-center mt-4">© evius life/kompass 2025. All rights reserved.</p>
       </div>
     </footer>
   );
